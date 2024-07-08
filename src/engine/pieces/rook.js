fcde5fa2eb4a5afa64688c824d642df8f54fb42e
Natasha Buckham <natashaisobel@gmail.com>
2024-07-08 15:20:40 +0100

    Red-3 tests for Rook passing
    Co-authored-by: Claudia Giancola <claudiaGiancola@users.noreply.github.com>

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +7,23 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let currentLocation = board.findPiece(this)
+        let currentXcoord = currentLocation.row
+        let currentYcoord = currentLocation.col
+
+        const rookMoves = [];
+        for (let i = 0; i <= 7 ; i++) {
+            if (i !== currentXcoord) {
+                rookMoves.push(Square.at(i, currentYcoord));
+            }
+        }
+
+        for (let i = 0; i <= 7 ; i++) {
+            if (i !== currentYcoord) {
+                rookMoves.push(Square.at(currentXcoord, i));
+            }
+        }
+
+        return rookMoves;
     }
 }
