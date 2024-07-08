097434d20910990323559ead778f0df244900275
Natasha Buckham <natashaisobel@gmail.com>
2024-07-08 14:19:51 +0100

    Makes test pass for white pawns moving one or two squares on first move

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -10,7 +10,11 @@ export default class Pawn extends Piece {
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         if (this.player === Player.WHITE) {
-            return [Square.at(location.row + 1, location.col)]
+            if (this.pieceHasMoved) {
+                return [Square.at(location.row + 1, location.col)]
+            } else {
+                return [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
+            }
         } else {
             return Square.at(location.row - 1, location.col)
         }
