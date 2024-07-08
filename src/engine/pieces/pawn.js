f9193cfeac01b205b3cf93df6e70bc9e4b6546fd
Natasha Buckham <natashaisobel@gmail.com>
2024-07-08 16:58:52 +0100

    "Red-8 fixed: pawns don't move if squares in front are blocked"
    Co-authored-by: Claudia Giancola <claudiaGiancola@users.noreply.github.com>

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -9,19 +9,34 @@ export default class Pawn extends Piece {
 
     getAvailableMoves(board) {
         let location = board.findPiece(this)
+
+        const spaceOneInFrontWhite = Square.at(location.row + 1, location.col);
+        const spaceTwoInFrontWhite = Square.at(location.row + 2, location.col);
+        const spaceOneInFrontBlack = Square.at(location.row - 1, location.col);
+        const spaceTwoInFrontBlack = Square.at(location.row - 2, location.col);
+
+        let availableMoves = [];
+
         if (this.player === Player.WHITE) {
-            if (this.pieceHasMoved) {
-                return [Square.at(location.row + 1, location.col)]
-            } else {
-                return [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
+            if (this.pieceHasMoved && !board.getPiece(spaceOneInFrontWhite)) {
+                availableMoves.push(Square.at(location.row + 1, location.col));
+            } else if (!this.pieceHasMoved && !board.getPiece(spaceOneInFrontWhite) && !board.getPiece(spaceTwoInFrontWhite)) {
+                availableMoves.push(
+                    Square.at(location.row + 1, location.col),
+                    Square.at(location.row + 2, location.col)
+                );
             }
         } else {
-            if (this.pieceHasMoved) {
-                return [Square.at(location.row - 1, location.col)]
-            } else {
-                return [Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col)]
-            }
-            
+            if (this.pieceHasMoved && !board.getPiece(spaceOneInFrontBlack)) {
+                availableMoves.push(Square.at(location.row - 1, location.col));
+            } else if (!this.pieceHasMoved && !board.getPiece(spaceOneInFrontBlack) && !board.getPiece(spaceTwoInFrontBlack)) {
+                availableMoves.push(
+                    Square.at(location.row - 1, location.col),
+                    Square.at(location.row - 2, location.col)
+                );
+            } 
         }
+
+        return availableMoves;
     }
 }
