44ecef32560d8a6918b0d9c38fea5580058bca59
Natasha Buckham <natashaisobel@gmail.com>
2024-07-09 11:38:49 +0100

    "Refactor red-3 and red-8"

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -5,38 +5,41 @@ import Piece from './piece';
 export default class Pawn extends Piece {
     constructor(player) {
         super(player);
+        this.availableMoves = [];
+        this.location;
+    }
+
+    addMove(x, y) {
+        this.availableMoves.push(Square.at(this.location.row + x, this.location.col + y));
+    }
+
+    isEmpty(board, x, y) {
+        return !board.getPiece(Square.at(this.location.row + x, this.location.col + y));
     }
 
     getAvailableMoves(board) {
-        let location = board.findPiece(this)
-
-        const spaceOneInFrontWhite = Square.at(location.row + 1, location.col);
-        const spaceTwoInFrontWhite = Square.at(location.row + 2, location.col);
-        const spaceOneInFrontBlack = Square.at(location.row - 1, location.col);
-        const spaceTwoInFrontBlack = Square.at(location.row - 2, location.col);
-
-        let availableMoves = [];
+        this.location = board.findPiece(this);
 
         if (this.player === Player.WHITE) {
-            if (this.pieceHasMoved && !board.getPiece(spaceOneInFrontWhite)) {
-                availableMoves.push(Square.at(location.row + 1, location.col));
-            } else if (!this.pieceHasMoved && !board.getPiece(spaceOneInFrontWhite) && !board.getPiece(spaceTwoInFrontWhite)) {
-                availableMoves.push(
-                    Square.at(location.row + 1, location.col),
-                    Square.at(location.row + 2, location.col)
-                );
+            if (!this.pieceHasMoved && this.isEmpty(board, 1, 0) && this.isEmpty(board, 2, 0)) { 
+                this.addMove(2, 0);
             }
-        } else {
-            if (this.pieceHasMoved && !board.getPiece(spaceOneInFrontBlack)) {
-                availableMoves.push(Square.at(location.row - 1, location.col));
-            } else if (!this.pieceHasMoved && !board.getPiece(spaceOneInFrontBlack) && !board.getPiece(spaceTwoInFrontBlack)) {
-                availableMoves.push(
-                    Square.at(location.row - 1, location.col),
-                    Square.at(location.row - 2, location.col)
-                );
-            } 
-        }
 
-        return availableMoves;
+            if (this.isEmpty(board, 1, 0)) {
+                this.addMove(1, 0);
+            } 
+        } else {
+            if (!this.pieceHasMoved && this.isEmpty(board, -1, 0) && this.isEmpty(board,-2, 0)) { 
+                this.addMove(-2, 0);
+            }
+
+            if (this.isEmpty(board, -1, 0)) {
+                this.addMove(-1, 0);
+            }
+        }
+        
+        //player.color !== player.color(piece in front) if we want it to be eaten
+
+        return this.availableMoves;
     }
 }
