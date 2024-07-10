3bd2247db5b84d4840b60d8b6fa5467c1748529d
Natasha Buckham <natashaisobel@gmail.com>
2024-07-10 11:33:11 +0100

    Red-13 tests passing for Rook taking correct pieces

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,5 +1,6 @@
 import Piece from './piece';
 import Square from '../square';
+import King from './king';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -16,6 +17,21 @@ export default class Rook extends Piece {
         return !board.getPiece(Square.at(x, y));
     }
 
+    isOpponent(board, x, y) {
+        return (
+            this.player !==
+            board.getPiece(Square.at(x, y)).player
+        );
+    }
+
+    isKing(board, x, y) {
+        return (
+            board.getPiece(
+                Square.at(x, y)
+            ) instanceof King
+        );
+    }
+
     getAvailableMoves(board) {
         this.location = board.findPiece(this);
         let currentXcoord = this.location.row;
@@ -26,6 +42,11 @@ export default class Rook extends Piece {
         for (let i = currentYcoord + 1; i <= 7; i++) {
             if (this.isEmpty(board, currentXcoord, i)) {
                 this.addMove(currentXcoord, i);
+                // if it's an opponent, you can take it, but then break
+            } else if (this.isOpponent(board, currentXcoord, i) && !this.isKing(board, currentXcoord, i)) {
+                this.addMove(currentXcoord, i);
+                break;
+                // if it's the same colour or a king, just break, you can't move through it
             } else {
                 break;
             }
@@ -34,6 +55,9 @@ export default class Rook extends Piece {
         for (let i = currentYcoord - 1; i >= 0; i--) {
             if (this.isEmpty(board, currentXcoord, i)) {
                 this.addMove(currentXcoord, i);
+            } else if (this.isOpponent(board, currentXcoord, i && !this.isKing(board, currentXcoord, i))) {
+                this.addMove(currentXcoord, i);
+                break;
             } else {
                 break;
             }
@@ -44,6 +68,9 @@ export default class Rook extends Piece {
         for (let i = currentXcoord - 1; i >= 0; i--) {
             if (this.isEmpty(board, i, currentYcoord)) {
                 this.addMove(i, currentYcoord);
+            } else if (this.isOpponent(board, i, currentYcoord) && !this.isKing(board, i, currentYcoord)) {
+                this.addMove(i, currentYcoord);
+                break;
             } else {
                 break;
             }
@@ -52,12 +79,14 @@ export default class Rook extends Piece {
         for (let i = currentXcoord + 1; i <= 7; i++) {
             if (this.isEmpty(board, i, currentYcoord)) {
                 this.addMove(i, currentYcoord);
+            } else if (this.isOpponent(board, i, currentYcoord) && !this.isKing(board, i, currentYcoord)) {
+                this.addMove(i, currentYcoord);
+                break;
             } else {
                 break;
             }
         }
 
-        return this.availableMoves;
         return this.availableMoves;
     }
 }
